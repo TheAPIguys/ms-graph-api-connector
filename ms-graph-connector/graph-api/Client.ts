f3c12636d4c3d904efce8715602129b9f11586b9
Daniel De Lucca <theAPIguys@outlook.com>
2023-11-04 10:17:10 +1300

    aJUSTMENT MADE TO MAKE JEST WORK PROPABLY WT PNPM

--- ms-graph-connector/graph-api/Client.ts
+++ ms-graph-connector/graph-api/Client.ts
@@ -1,27 +1,27 @@
-import { ClientSecretCredential } from '@azure/identity';
-import { Client } from '@microsoft/microsoft-graph-client';
+import { ClientSecretCredential } from '@azure/identity'
+import { Client } from '@microsoft/microsoft-graph-client'
 import {
   TokenCredentialAuthenticationProvider,
   TokenCredentialAuthenticationProviderOptions,
-} from '@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials';
+} from '@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials'
 
 export async function InitGraphClient() {
   const tokenCredential = new ClientSecretCredential(
     '71124398-4b74-4832-b48a-1c01fb46e497', // TENANT_ID  process.env.TENANT_ID ? process.env.TENANT_ID :
     'a4318a83-8311-407d-b571-768aff5588de', // CLIENT_ID process.env.CLIENT_ID ? process.env.CLIENT_ID :
     'rid8Q~hZ13OKFa0HgH5ceYdyCzXv5Nm4HR_A-bLr',
-  );
+  )
 
   // Set your scopes and options for TokenCredential.getToken (Check the ` interface GetTokenOptions` in (TokenCredential Implementation)[https://github.com/Azure/azure-sdk-for-js/blob/master/sdk/core/core-auth/src/tokenCredential.ts])
 
   const options: TokenCredentialAuthenticationProviderOptions = {
     scopes: ['https://graph.microsoft.com/.default'],
-  };
+  }
 
   // Create an instance of the TokenCredentialAuthenticationProvider by passing the tokenCredential instance and options to the constructor
-  const authProvider = new TokenCredentialAuthenticationProvider(tokenCredential, options);
+  const authProvider = new TokenCredentialAuthenticationProvider(tokenCredential, options)
   return Client.initWithMiddleware({
     debugLogging: true,
     authProvider: authProvider,
-  });
+  })
 }
